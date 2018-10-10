382216dfc7b18ccb9599f9afc290e99722b315d0
Joanna Charzynska <joannacharzynska@hotmail.com>
2018-10-10 17:32:10 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,14 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    { byte age = "28";
+    String hometown = "rumia";
+        System.out.println(age + "\n" + hometown);
+    }
+}
